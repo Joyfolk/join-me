aa8b81b79f4a661af64bfab5093b347b3e176527
Mikhail Turnovskiy <joyfolk@gmail.com>
2019-08-05 23:15:29 +0300

    external sorting fix + basic tests

--- src/main/scala/join/ExternalSort.scala
+++ src/main/scala/join/ExternalSort.scala
@@ -2,13 +2,13 @@ package join
 import scala.collection.mutable
 
 object ExternalSort {
-  implicit val ord: Ordering[Data] = Row.ordering[Data#Key, Data]
+  implicit val ord: Ordering[Data] = Row.ordering[Data#Key, Data].reverse
   implicit val oracle: Oracle = null
 
   def sort[H](source: H, target: H)(implicit dataIO: DataIO[H]): Unit =
     joinSort(splitSort(source), dataIO.dataWriter(target))
 
-  private[this] def joinSort[H](parts: IndexedSeq[DataReader[H]], output: DataWriter[H, Data])(
+  private[join] def joinSort[H](parts: IndexedSeq[DataReader[H]], output: DataWriter[H, Data])(
       implicit ordData: Ordering[Data]): Unit = {
     implicit val ord: Ordering[(Data, Int)] = Ordering.by(_._1)
     val heap = mutable.PriorityQueue[(Data, Int)]()
@@ -16,6 +16,7 @@ object ExternalSort {
     iterators.zipWithIndex.foreach {
       case (iter, i) if iter.hasNext =>
         heap.enqueue((iter.next(), i))
+      case _ => // ignore
     }
     while (heap.nonEmpty) {
       val (d, i) = heap.dequeue()
@@ -25,7 +26,16 @@ object ExternalSort {
     }
   }
 
-  private[this] def splitSort[H](
+  private[this] def dumpHeap[H](heap: mutable.PriorityQueue[Data])(implicit dataIO: DataIO[H]): DataWriter[H, Data] = {
+    val w = dataIO.temporaryWriter
+    try {
+      w.writeAll(heap.dequeueAll)
+      System.gc()
+      w
+    } finally w.close()
+  }
+
+  private[join] def splitSort[H](
       source: H)(implicit dataIO: DataIO[H], oracle: Oracle, ord: Ordering[Data]): IndexedSeq[DataReader[H]] = {
     val heap = mutable.PriorityQueue[Data]()
     var parts = IndexedSeq[DataWriter[H, Data]]()
@@ -35,12 +45,12 @@ object ExternalSort {
         if (!oracle.underMemoryPressure)
           heap.enqueue(d)
         else {
-          val w = dataIO.temporaryWriter
-          w.writeAll(heap.dequeueAll)
-          System.gc()
+          val w = dumpHeap(heap)
           parts = parts :+ w
         }
       }
+      if (heap.nonEmpty)
+        parts = parts :+ dumpHeap(heap)
       parts.map(_.handle).map(dataIO.reader)
     } finally reader.close()
   }
---
+++ src/test/scala/join/ExternalSortTest.scala
@@ -0,0 +1,71 @@
+package join
+import java.io.{File, PrintWriter}
+
+import org.scalatest.FunSuite
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+
+class ExternalSortTest extends FunSuite {
+  implicit val ord: Ordering[Data] = Ordering.by[Data, Data#Key](_.key).reverse
+  implicit val dataIO: FileIO = new FileIO
+
+  test("splitSort: Sort single file") {
+    implicit val oracle: Oracle = Oracle()
+    val f = File.createTempFile("test_", ".csv")
+    val w = new PrintWriter(f)
+    try {
+      w.println("5\tLondon")
+      w.println("4\tMoscow")
+      w.println("3\tParis")
+      w.println("42\tMagadan")
+    } finally w.close()
+    val res = ExternalSort.splitSort(f)
+    assert(res.size == 1)
+    val s = Source.fromFile(res(0).handle)
+    try {
+      assert(
+        s.getLines().map(Row.fromString).toSeq == Seq(Data(3L, "Paris"),
+                                                      Data(4L, "Moscow"),
+                                                      Data(5L, "London"),
+                                                      Data(42L, "Magadan")))
+    } finally s.close
+  }
+
+  class MockDataWriter extends DataWriter[Unit, Data] {
+    val buf: ArrayBuffer[Data] = ArrayBuffer()
+    override def write(r: Data): Unit = buf.append(r)
+    override def handle: Unit = ()
+    override def close(): Unit = {}
+  }
+
+  class MockDataReader(val s: Seq[Data]) extends DataReader[Unit] {
+    override def iterator: BufferedIterator[Data] = s.iterator.buffered
+    override def handle: Unit = ()
+    override def close(): Unit = {}
+  }
+
+  test("joinSort") {
+    val w = new MockDataWriter
+    ExternalSort.joinSort(
+      IndexedSeq(
+        new MockDataReader(Seq(Data(1L, "a1"), Data(5L, "a5"), Data(9L, "a9"))),
+        new MockDataReader(Seq(Data(2L, "b2"), Data(3L, "b3"), Data(4L, "b4"), Data(5L, "b5"))),
+        new MockDataReader(Seq(Data(5L, "c5"))),
+        new MockDataReader(Seq())
+      ),
+      w
+    )
+    assert(
+      w.buf == Seq(
+        Data(1L, "a1"),
+        Data(2L, "b2"),
+        Data(3L, "b3"),
+        Data(4L, "b4"),
+        Data(5L, "a5"),
+        Data(5L, "b5"),
+        Data(5L, "c5"),
+        Data(9L, "a9")
+      ))
+  }
+}
